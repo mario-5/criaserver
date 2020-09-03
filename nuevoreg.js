980739bd14b4c123db5518a85bf2954ba7927d9d
Mario <32851760+mario-5@users.noreply.github.com>
2020-09-03 16:23:43 -0300

    Initial comunication setup

---
+++ nuevoreg.js
@@ -0,0 +1,39 @@
+//adquiere las librerias necesarias
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+//const sqlite3 = require('sqlite3').verbose();
+
+
+// La función tiene como parámetro el request y el response.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Define la respuesta
+let respuesta = {
+ error: false,
+ codigo: 200,
+ mensaje: ''
+};
+
+app.post('/', function (req, res) {
+  console.log("Página de inicio...")
+
+let temp="";
+let hum="";
+if(!req.body.temperatura) {
+  temp='sin registro';
+ } else{
+  temp=req.body.temperatura;
+  hum=req.body.humedad;
+ };
+ console.log(temp);
+ console.log(hum);
+ res.send(respuesta);
+ });
+// open the database
+
+// Correr el servidor con el puerto 8999.
+app.listen(9000, () => {
+ console.log("El servidor está inicializado en el puerto 9000");
+ });
